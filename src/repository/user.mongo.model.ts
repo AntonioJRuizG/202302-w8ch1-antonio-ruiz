85ef85b682644b1d6a7f97fc2617a31038ad3eb2
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-07 00:37:53 +0100

    Fix app import path and add mongoose model

---
+++ src/repository/user.mongo.model.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from 'mongoose';
+import { User } from '../entities/user';
+
+const userSchema = new Schema<User>({
+  name: {
+    type: String,
+    requiered: true,
+  },
+  password: {
+    type: String,
+    requierd: true,
+  },
+  friends: [{ type: Schema.Types.ObjectId, ref: 'Member' }],
+  enemies: [{ type: Schema.Types.ObjectId, ref: 'Member' }],
+});
+
+userSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject._id;
+    delete returnedObject.password;
+  },
+});
+
+export const UserModel = model('User', userSchema, 'users');
